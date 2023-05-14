c0f6c541259635c6bf4caf0da9b276bf55559055
manivannan-robot <manivannan@dev-bee.com>
2023-05-14 12:13:28 +0530

    email duplication deleted

--- lettings.php
+++ lettings.php
@@ -25,24 +25,35 @@ $worksheet = $spreadsheet->getActiveSheet();
 $highestRow = $worksheet->getHighestRow();
 $highestColumn = $worksheet->getHighestColumn();
 
-for ($row = 2; $row <= $highestRow; $row++) {
-  $sValue = $worksheet->getCell('U' . $row)->getValue();
-  if(!empty($sValue)){
-   
-        $wValue = $worksheet->getCell('U' . $row)->getValue();
-        $xValue = $worksheet->getCell('V' . $row)->getValue();
-        $yValue = $worksheet->getCell('W' . $row)->getValue();
-      
-        // Insert data into MySQL database
-        $sql = "INSERT INTO clients (`name`, `email`, `contact_no`,`type`) VALUES ('$wValue', '$xValue', '$yValue','Tenant')";
+    for ($row = 2; $row <= $highestRow; $row++) {
+      $sValue = $worksheet->getCell('S' . $row)->getValue();
+      if (!empty($sValue)) {
 
-        if ($conn->query($sql) === TRUE) {
-          echo "New Tenant created successfully"."<br>";
+        $uValue = $worksheet->getCell('U' . $row)->getValue();
+        $vValue = $worksheet->getCell('V' . $row)->getValue();
+        $wValue = $worksheet->getCell('W' . $row)->getValue();
+
+        // Check if the email already exists in the database
+        $emailExistsQuery = "SELECT COUNT(*) as count FROM clients WHERE email = '$vValue'";
+        $emailExistsResult = $conn->query($emailExistsQuery);
+        $emailExistsData = $emailExistsResult->fetch_assoc();
+        $emailCount = $emailExistsData['count'];
+
+        if ($emailCount > 0) {
+          echo "Skipped - Email already exists: $vValue<br>";
         } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
+          // Insert data into MySQL database
+          $sql = "INSERT INTO clients (`name`, `email`, `contact_no`, `type`) VALUES ('$uValue', '$vValue', '$wValue', 'Tenant')";
+
+          if ($conn->query($sql) === TRUE) {
+            echo "New Tenant created successfully<br>";
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
         }
-   }
-  }
+      }
+    }
+
 
   // Loop through each row of the worksheet
 for ($row = 2; $row <= $highestRow; $row++) {
--- properties.php
+++ properties.php
@@ -49,14 +49,19 @@ for ($row = 2; $row <= $highestRow; $row++) {
   $fValue = $worksheet->getCell('F' . $row)->getValue();
   $gValue = $worksheet->getCell('G' . $row)->getValue();
   $hValue = $worksheet->getCell('H' . $row)->getValue();
-  $iValue = $worksheet->getCell('I' . $row)->getValue();
+  $property_price = $worksheet->getCell('I' . $row)->getValue();
   $jValue = $worksheet->getCell('J' . $row)->getValue();
+  $deposit=$property_price*$jValue;
   $mValue = $worksheet->getCell('M' . $row)->getValue();
+  $commissionPercentage = $worksheet->getCell('L' . $row)->getValue();
+  $rent = $worksheet->getCell('K' . $row)->getValue();
+  $commission = $rent * ($commissionPercentage / 100);
+
   $tValue = $worksheet->getCell('T' . $row)->getValue();
 
   // Insert data into MySQL database
   $sql = "INSERT INTO properties (`property_ref`, `address_line1`, `address_line2`,`city`,`county`,`postcode`, `property_category`, `property_class`,`property_price`,`deposit`, `commission`,`landlord_id`,`tenant_buyer_id`) 
-                          VALUES ('$aValue', '$bValue', '$cValue','$dValue', '$eValue','$fValue', '$gValue', '$hValue','$iValue', '$jValue', '$mValue','$last_inserted_id','$tValue')";
+                          VALUES ('$aValue', '$bValue', '$cValue','$dValue', '$eValue','$fValue', '$gValue', '$hValue','$property_price', '$deposit', '$commission','$last_inserted_id','$tValue')";
 
   if ($conn->query($sql) === TRUE) {
     echo "New landlord created successfully"."<br>";
@@ -69,17 +74,29 @@ for ($row = 2; $row <= $highestRow; $row++) {
     $sValue = $worksheet->getCell('S' . $row)->getValue();
     
     if(empty($sValue)){
-            $wValue = $worksheet->getCell('U' . $row)->getValue();
-            $xValue = $worksheet->getCell('V' . $row)->getValue();
-            $yValue = $worksheet->getCell('W' . $row)->getValue();
-        
-            // Insert data into MySQL database
-            $sql = "INSERT INTO clients (`name`, `email`, `contact_no`,`type`) VALUES ('$wValue', '$xValue', '$yValue','buyer')";
-            if ($conn->query($sql) === TRUE) {
-            echo "New buyer created successfully"."<br>";
-            } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            }
+      $uValue = $worksheet->getCell('U' . $row)->getValue();
+      $vValue = $worksheet->getCell('V' . $row)->getValue();
+      $wValue = $worksheet->getCell('W' . $row)->getValue();
+  
+      // Check if the email already exists in the database
+      $emailExistsQuery = "SELECT COUNT(*) as count FROM clients WHERE email = '$vValue'";
+      $emailExistsResult = $conn->query($emailExistsQuery);
+      $emailExistsData = $emailExistsResult->fetch_assoc();
+      $emailCount = $emailExistsData['count'];
+  
+      if ($emailCount > 0) {
+        echo "Skipped - Email already exists: $vValue<br>";
+      } else {
+        // Insert data into MySQL database
+        $sql = "INSERT INTO clients (`name`, `email`, `contact_no`, `type`) VALUES ('$uValue', '$vValue', '$wValue', 'buyer')";
+  
+        if ($conn->query($sql) === TRUE) {
+          echo "New Buyer created successfully<br>";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+      }
+
     }
   }
 
